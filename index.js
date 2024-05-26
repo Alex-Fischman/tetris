890c99f5b76d83d914e83087090af5293dcda6fa
Alex Fischman <alexdfischman@gmail.com>
2024-05-26 15:17:58 -0700

    Piece drawing, movement, and rotation

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const FAST_DROP_BUTTON_OFFSET = 0.3;
 const BOARD_COLOR = "#111";
 const BACKGROUND_COLOR = "#EEE";
 const BUTTON_COLOR = "#DDD";
+const PIECE_COLORS = ["#1EE", "#11E", "#EA1", "#EE1", "#1E1", "#E1E", "#E11"];
 
 let context = document.getElementById("canvas").getContext("2d");
 let stroke =         style  => { context.strokeStyle = style; context.stroke();          };
@@ -32,17 +33,83 @@ let fillRect   = (rect, style) => {
 	context.fillRect(rect.x, rect.y, rect.w, rect.h);
 };
 
-let move_left = () => { console.log("move_left"); };
-let move_right = () => { console.log("move_right"); };
-let turn_left = () => { console.log("turn_left"); };
-let turn_right = () => { console.log("turn_right"); };
-let fast_drop = () => { console.log("fast_drop"); };
+let mod = (n, m) => ((n % m) + m) % m;
+
+let pieces = [
+	[[3, 0], [4, 0], [5, 0], [6, 0]],
+	[[3, 0], [3, -1], [4, 0], [5, 0]],
+	[[3, 0], [4, 0], [5, 0], [5, -1]],
+	[[4, 0], [5, 0], [4, -1], [5, -1]],
+	[[3, 0], [4, 0], [4, -1], [5, -1]],
+	[[3, 0], [4, 0], [5, 0], [4, -1]],
+	[[3, -1], [4, -1], [4, 0], [5, 0]],
+];
+let centers = [[4.5, 0.5], [4, 0], [4, 0], [4.5, -0.5], [4, 0], [4, 0], [4, 0]];
+
+let active_piece = {
+	x: 0,
+	y: 4,
+	r: 0,
+	i: 0,
+};
+
+let get_active_cells = () => pieces[active_piece.i].map(([x, y]) => {
+	let c = centers[active_piece.i];
+	let theta = Math.PI / 2 * active_piece.r;
+	let cos = Math.round(Math.cos(theta));
+	let sin = Math.round(Math.sin(theta));
+	return [
+		(cos * (x - c[0]) - sin * (y - c[1])) + c[0] + active_piece.x,
+		(sin * (x - c[0]) + cos * (y - c[1])) + c[1] + active_piece.y
+	];
+});
+
+let is_active_colliding = () => {
+	for (let [x, y] of get_active_cells()) {
+		if (x < 0 || x >= BOARD_COLUMNS || y >= BOARD_ROWS) return true;
+		if (static_board[y][x] != BOARD_COLOR) return true;
+	}
+	return false;
+};
+
+let static_board = [];
+for (let i = 0; i < BOARD_ROWS; i++) {
+	let row = [];
+	for (let j = 0; j < BOARD_COLUMNS; j++) row.push(BOARD_COLOR);
+	static_board.push(row);
+}
+
+let move_left = () => {
+	active_piece.x -= 1;
+	if (is_active_colliding()) active_piece.x += 1;
+};
+let move_right = () => {
+	active_piece.x += 1;
+	if (is_active_colliding()) active_piece.x -= 1;
+};
+let fast_drop = () => {
+	while (!is_active_colliding()) active_piece.y += 1;
+	active_piece.y -= 1;
+};
+let turn_left = () => {
+	active_piece.r = mod(active_piece.r - 1, 4);
+	if (is_active_colliding()) active_piece.r = mod(active_piece.r + 1, 4);
+};
+let turn_right = () => {
+	active_piece.r = mod(active_piece.r + 1, 4);
+	if (is_active_colliding()) active_piece.r = mod(active_piece.r - 1, 4);
+};
 let pause_game = () => { console.log("pause_game"); };
 
-let button_rects = [undefined, undefined, undefined,  undefined, undefined,  undefined];
-let button_funcs = [move_left, turn_left, pause_game, fast_drop, turn_right, move_right]
+let button_rects = [undefined, undefined, undefined,  undefined, undefined,  undefined ];
+let button_keys  = ["KeyA",    "KeyJ",    "Escape",   "Space",   "KeyL",     "KeyD"    ];
+let button_funcs = [move_left, turn_left, pause_game, fast_drop, turn_right, move_right];
+
 document.addEventListener("pointerdown", e => button_rects.map((r, i) => {
 	if (rect_point(r, e.x, e.y)) button_funcs[i]();
+}));
+document.addEventListener("keydown", e => button_keys.map((k, i) => {
+	if (e.code == k) button_funcs[i]();
 }));
 
 let update = dt => {};
@@ -70,17 +137,21 @@ let render = () => {
 		board = rect(bx, by, bw, bh);
 	}
 
-	fillRect(board, BOARD_COLOR);
-
-	for (let i = 0; i < BOARD_COLUMNS; i++) for (let j = 0; j < BOARD_ROWS; j++) {
+	let drawCell = (i, j, style) => {
 		let cell = rect(
 			fx(board, i / BOARD_COLUMNS),
 			fy(board, j / BOARD_ROWS),
 			board.w / BOARD_COLUMNS,
 			board.h / BOARD_ROWS
 		);
+		fillRect(cell, style);
 		strokeRect(cell, BACKGROUND_COLOR);
-	}
+	};
+
+	for (let i = 0; i < BOARD_COLUMNS; i++) for (let j = 0; j < BOARD_ROWS; j++)
+		drawCell(i, j, static_board[j][i]);
+
+	for (let [x, y] of get_active_cells()) drawCell(x, y, PIECE_COLORS[active_piece.i]);
 
 	// Draw controls
 	let controls = rect(
