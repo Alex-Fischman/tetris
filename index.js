ecbb19b38a6f99b9df2625837c48aea328bb306e
Alex Fischman <alexdfischman@gmail.com>
2024-05-26 22:54:16 -0700

    Cleanup, add control line

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ let colors = (a, b, c) => [
 	`#${a}${c}${c}`, `#${a}${a}${c}`, `#${c}${b}${a}`, `#${c}${c}${a}`,
 	`#${a}${c}${a}`, `#${c}${a}${c}`, `#${c}${a}${a}`,
 ];
-
 const PIECE_COLORS = colors("1", "A", "E");
 const GHOST_COLORS = colors("A", "C", "E");
 const BOARD_COLOR = "#111";
+const LINE_COLOR = "#FFF1";
 
 let active_piece = { x: 0, y: 0, r: 0, i: 0 };
 let bag = [];
@@ -49,43 +49,14 @@ let get_active_cells = () => pieces[active_piece.i].map(([x, y]) => {
 
 let is_active_colliding = () => {
 	for (let [x, y] of get_active_cells()) {
-		if (x < 0 || x >= 10 || y >= 20) return true;
-		if (y < 0) continue; // some pieces start above the board
-		if (stack[y][x] != BOARD_COLOR) return true;
+		if (x < 0 || x >= 10 || y >= 20)          return true;
+		if (y >= 0 && stack[y][x] != BOARD_COLOR) return true;
 	}
 	return false;
 };
 
-let try_kicks = () => {
-	if (!is_active_colliding()) return true;
-
-	active_piece.x += 1;
-	if (!is_active_colliding()) return true;
-	active_piece.x -= 1;
-
-	active_piece.x -= 1;
-	if (!is_active_colliding()) return true;
-	active_piece.x += 1;
-
-	active_piece.y += 1;
-	if (!is_active_colliding()) return true;
-	active_piece.y -= 1;
-
-	active_piece.y -= 1;
-	if (!is_active_colliding()) return true;
-	active_piece.y += 1;
-
-	return false;
-};
-
-let fast_drop = () => {
-	while (!is_active_colliding()) active_piece.y += 1;
-	active_piece.y -= 1;
-};
-
 let lines = 0;
 let last_drop_time = performance.now();
-
 let row_time = () => Math.pow((0.8 - ((lines / 10) * 0.007)), (lines / 10)) * 1000;
 
 let piece_has_landed = () => {
@@ -105,7 +76,6 @@ let piece_has_landed = () => {
 	}
 };
 
-/// Controls
 let move_left = () => {
 	active_piece.x -= 1;
 	if (is_active_colliding()) active_piece.x += 1;
@@ -115,7 +85,9 @@ let move_right = () => {
 	if (is_active_colliding()) active_piece.x -= 1;
 };
 let hard_drop = () => {
-	fast_drop();
+	while (!is_active_colliding()) active_piece.y += 1;
+	active_piece.y -= 1;
+
 	piece_has_landed();
 };
 let turn_right = () => {
@@ -123,10 +95,27 @@ let turn_right = () => {
 	let mod = (n, m) => ((n % m) + m) % m;
 
 	active_piece.r = mod(active_piece.r + 1, 4);
-	if (!try_kicks()) active_piece.r = mod(active_piece.r - 1, 4);
+	if (!is_active_colliding()) return;
+
+	active_piece.x += 1;
+	if (!is_active_colliding()) return;
+	active_piece.x -= 1;
+
+	active_piece.x -= 1;
+	if (!is_active_colliding()) return;
+	active_piece.x += 1;
+
+	active_piece.y += 1;
+	if (!is_active_colliding()) return;
+	active_piece.y -= 1;
+
+	active_piece.y -= 1;
+	if (!is_active_colliding()) return;
+	active_piece.y += 1;
+
+	active_piece.r = mod(active_piece.r - 1, 4);
 };
 
-/// Input handlers
 let button_rects = [undefined, undefined, undefined,  undefined ];
 let button_keys  = ["Space",   "KeyA",    "KeyW",     "KeyD"    ];
 let button_funcs = [hard_drop, move_left, turn_right, move_right];
@@ -161,11 +150,6 @@ let render = () => {
 	context.fillStyle = BOARD_COLOR;
 	context.fillRect(0, 0, w, h);
 
-	button_rects[0] = { x: 0,       y: 0,     w: w,     h: h / 2 };
-	button_rects[1] = { x: w * 0/3, y: h / 2, w: w / 3, h: h / 2 };
-	button_rects[2] = { x: w * 1/3, y: h / 2, w: w / 3, h: h / 2 };
-	button_rects[3] = { x: w * 2/3, y: h / 2, w: w / 3, h: h / 2 };
-
 	let bw = h / 2;
 	let bh = h;
 	let bx = w / 2 - bw / 2;
@@ -178,12 +162,24 @@ let render = () => {
 	for (let i = 0; i < 10; i++) for (let j = 0; j < 20; j++) drawCell(i, j, stack[j][i]);
 
 	let saved_y = active_piece.y;
-	fast_drop();
+	while (!is_active_colliding()) active_piece.y += 1;
+	active_piece.y -= 1;
 	let ghost_cells = get_active_cells();
 	active_piece.y = saved_y;
 
 	for (let [x, y] of ghost_cells)        drawCell(x, y, GHOST_COLORS[active_piece.i]);
 	for (let [x, y] of get_active_cells()) drawCell(x, y, PIECE_COLORS[active_piece.i]);
+
+	button_rects[0] = { x: 0,       y: 0,       w: w,     h: h * 2/3 };
+	button_rects[1] = { x: w * 0/3, y: h * 2/3, w: w / 3, h: h * 1/3 };
+	button_rects[2] = { x: w * 1/3, y: h * 2/3, w: w / 3, h: h * 1/3 };
+	button_rects[3] = { x: w * 2/3, y: h * 2/3, w: w / 3, h: h * 1/3 };
+
+	context.moveTo(0, h * 2/3);
+	context.lineTo(w, h * 2/3);
+	context.strokeStyle = LINE_COLOR;
+	context.lineWidth = 5;
+	context.stroke();
 };
 
 let then = performance.now();
